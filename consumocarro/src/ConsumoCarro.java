904dfff045375b7870cb879ae46b2d61ed184d75
Mateus Carestiato <mateuspcdaniel@gmail.com>
2025-08-28 10:57:37 -0300

    Correcao de ortografia

--- consumocarro/src/ConsumoCarro.java
+++ consumocarro/src/ConsumoCarro.java
@@ -10,6 +10,6 @@ public class ConsumoCarro {
         System.out.println("Informe a quantidade de gasolina consumida (em litros): ");
         double gasolina = teclado.nextDouble();
         System.out.println("Velocidade média (km/h): " + distancia/tempo);
-        System.out.println("Consumo de combustível(km/l): " + distancia/gasolina);
+        System.out.println("Consumo de combustível (km/l): " + distancia/gasolina);
     }
 }
